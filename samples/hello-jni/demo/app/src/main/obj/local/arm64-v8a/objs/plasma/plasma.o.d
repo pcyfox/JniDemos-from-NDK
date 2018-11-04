367607cadffe0e43e0896be584e2eb1667a1aa8d
panchengyao <panchengyao@eims.com.cn>
2018-11-04 09:51:30 +0800

    添加工程文件

---
+++ samples/hello-jni/demo/app/src/main/obj/local/arm64-v8a/objs/plasma/plasma.o.d
@@ -0,0 +1,2 @@
+/Users/pcyfox/AndroidStudioProjects/JNI_demo/samples/bitmap-plasma/bitmapplasma/app/src/main/obj/local/arm64-v8a/objs/plasma/plasma.o: \
+  /Users/pcyfox/AndroidStudioProjects/JNI_demo/samples/bitmap-plasma/bitmapplasma/app/src/main/jni/plasma.c
---
+++ samples/hello-jni/demo/app/src/main/obj/local/armeabi-v7a/objs/plasma/plasma.o.d
@@ -0,0 +1,2 @@
+/Users/pcyfox/AndroidStudioProjects/JNI_demo/samples/bitmap-plasma/bitmapplasma/app/src/main/obj/local/armeabi-v7a/objs/plasma/plasma.o: \
+  /Users/pcyfox/AndroidStudioProjects/JNI_demo/samples/bitmap-plasma/bitmapplasma/app/src/main/jni/plasma.c
